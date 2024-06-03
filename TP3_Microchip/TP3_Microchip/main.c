5d2965cf27b15c7511bb5de17570a1b56c985bf4
santiago <santiago.entraigas@alu.ing.unlp.edu.ar>
2024-06-03 14:31:07 -0300

    initial commit

---
+++ TP3_Microchip/TP3_Microchip/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP3_Microchip.c
+ *
+ * Created: 3/6/2024 14:19:32
+ * Author : Barcala
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
