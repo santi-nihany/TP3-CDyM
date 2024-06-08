405164a1aa03ed70bc434a115d2c0f10f89ae86a
TobiasGI <tobiasgarciaiacovelli@gmail.com>
2024-06-08 19:22:27 -0300

    Mostrar datos en el lcd

--- TP3_Microchip/TP3_Microchip/main.c
+++ TP3_Microchip/TP3_Microchip/main.c
@@ -20,15 +20,11 @@ int main(void)
 	uint8_t humedad_dec;
 	
 	DHT11_init();
+	UART_init();
+	
 	_delay_ms(10);
 	LCD_Init();
 	_delay_ms(100);
-	
-	LCDclr();
-	_delay_ms(100);
-
-	UART_init();
-
 	
 	while (1) {
 		uint8_t status = DHT11_read(&temperatura_int, &temperatura_dec ,&humedad_int, &humedad_dec );
@@ -36,6 +32,20 @@ int main(void)
 			LCDclr();
 			printf("Temperatura = %d.%d\n\r", temperatura_int, temperatura_dec);
 			printf("Humedad = %d.%d\n\r", humedad_int, humedad_dec);
+			
+			LCDGotoXY(0,0);
+			LCDstring("Temp: ",strlen("Temp: "));
+			LCDescribeDato(temperatura_int,2);
+			LCDstring(".",strlen("."));
+			LCDescribeDato(temperatura_dec,2);
+			LCDstring(" C",strlen(" C"));
+			LCDGotoXY(0,1);
+			LCDstring("Hum: ",strlen("Hum: "));
+			LCDescribeDato(humedad_int,2);
+			LCDstring(".",strlen("."));
+			LCDescribeDato(humedad_dec,2);
+			LCDstring(" %",strlen(" %"));
+			
 			_delay_ms(100);
 			/*sprintf(temperatura_int, "%d", temperatura_int);
 			sprintf(humedad_int, "%d", humedad_int);
